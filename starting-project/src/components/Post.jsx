ac94c583edf8d230a40c3143be83e773741224d5
Gr1mm4g3 <grimmage@protonmail.com>
2024-12-16 16:41:14 +0100

    Outputting dynamic values

--- starting-project/src/components/Post.jsx
+++ starting-project/src/components/Post.jsx
@@ -1,7 +1,15 @@
+const names = ['Maximilian', 'Manuel'];
+
 function Post(){
+
+    // Component logic
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+
     return (
         <div>
-            <p>Lesley</p>
+            {/*<p>{2+2}</p>*/}
+            <p>{chosenName}</p>
             <p>React.js is awesome!</p>
         </div>
     );
