724b0c1447e6e06327241d5ae37a2317675fcb84
Gr1mm4g3 <grimmage@protonmail.com>
2024-12-16 16:54:23 +0100

    Passing data to components with props

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,12 +1,12 @@
 import Post from "./components/Post";
 
 function App() {
-  return <>
+  return <main>
+    <Post author="Lesley" body="React.js is awesome!"/>
+    <Post author="Manuel" body="Check out the full course!"/>
     <Post />
     <Post />
-    <Post />
-    <Post />
-  </>
+  </main>
 }
 
 export default App;
--- starting-project/src/components/Post.jsx
+++ starting-project/src/components/Post.jsx
@@ -1,16 +1,9 @@
-const names = ['Maximilian', 'Manuel'];
-
-function Post(){
-
-    // Component logic
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post(props){
 
     return (
         <div>
-            {/*<p>{2+2}</p>*/}
-            <p>{chosenName}</p>
-            <p>React.js is awesome!</p>
+            <p>{props.author}</p>
+            <p>{props.body}</p>
         </div>
     );
 }
