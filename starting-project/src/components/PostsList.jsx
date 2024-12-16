80018f2ade6507fc0ebfc8cdb2dde8fd65c5ab9c
Gr1mm4g3 <grimmage@protonmail.com>
2024-12-16 22:54:42 +0100

    Preparing the app for state management.

--- starting-project/src/components/PostsList.jsx
+++ starting-project/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from './NewPost'
 import Post from './Post'
 import styles from './PostsList.module.css'
 
 function PostsList() {
   return (
-    <ul className={styles.posts}>
-      <Post author="Lesley" body="React.js is awesome!"/>
-      <Post author="Manuel" body="Check out the full course!"/>
-    </ul>
+    <>
+      <NewPost />
+      <ul className={styles.posts}>
+        <Post author="Lesley" body="React.js is awesome!"/>
+        <Post author="Manuel" body="Check out the full course!"/>
+      </ul>
+    </>
   )
 }
 
